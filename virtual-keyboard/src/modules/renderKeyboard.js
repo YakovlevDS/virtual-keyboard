2a030955e291f28e1906d18cc3fd33cae6a3de4d
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-05-05 21:48:53 +0300

    feat: add clear

--- virtual-keyboard/src/modules/renderKeyboard.js
+++ virtual-keyboard/src/modules/renderKeyboard.js
@@ -5,17 +5,18 @@ import Row from './renderRow';
 import Btn from './renderBtn';
 
 const renderKeyboard = (bool) => {
+  const outputArea = document.querySelector('.field-out__text');
   let isShift = false;
   let isAlt = false;
   let isCtrl = false;
   let isCaps = false;
   const btnPress = {};
-  let arrButtons = [];
+  const arrButtons = [];
   const lang = bool ? en : ru;
 
   const input = document.createElement('div');
   input.classList.add('field-input');
-  document.querySelector('.field-out').after(input);
+  outputArea.after(input);
 
   for (let i = 1; i < 6; i += 1) {
     new Row(i).render();
@@ -43,11 +44,12 @@ const renderKeyboard = (bool) => {
 
   init();
 
-  const allBtns = document.querySelectorAll('.input__key')
+  const allBtns = document.querySelectorAll('.input__key');
 
-  allBtns.forEach(b => arrButtons.push({
+  allBtns.forEach((b) => arrButtons.push({
     divUp: b.children[0],
     divMain: b.children[1],
+    divBtn: b,
     id: b.dataset.id,
     code: b.dataset.code,
     isFn: b.dataset.isfn,
@@ -58,97 +60,78 @@ const renderKeyboard = (bool) => {
 
   // console.dir(document.querySelectorAll('.input__key')[0].children[0].innerHTML);
   // console.dir(document.querySelectorAll('.input__key')[0].dataset);
+  // console.dir(document.querySelector('.input__key'));
 
   // arrButtons = arrButtons.push(Array.from(document.querySelectorAll('.input__key')));
   // console.log(arrButtons);
 
-  // const upCase = (isUp) => {
-  //   if (isUp) {
-  //     arrButtons.forEach((i) => {
-  //       console.log(i.down.innerHTML.toUpperCase());
-
-  //       i.down.innerHTML.toUpperCase();
-  //       i.upper.classList.add('hide');
-  //     });
-  //   } else {
-  //     arrButtons.forEach((i) => {
-  //       i.down.TextContent.toLowerCase();
-  //       i.upper.classList.remove('hide');
-  //       if (i.sup) {
-  //         i.down.classList.add('hide');
-  //         i.upper.classList.add('up-active');
-  //       }
-  //     });
-  //   }
-  // };
-
-  if (isUp) {
-    arrButtons.forEach((i) => {
-      if (i.sup) {
-        if (isShift) {
-          i.upper.classList.add('up-active');
-          i.down.classList.add('up-inactive');
+  const upCase = (isUp = false) => {
+    if (isUp) {
+      arrButtons.forEach((i) => {
+        if (i.divUp) {
+          if (isShift) {
+            i.divUp.classList.add('up-active');
+            i.divMain.classList.add('up-inactive');
+          }
         }
-      }
-      if (!i.isFn && isCaps && !isShift && !i.upper.innerHTML) {
-        i.down.TextContent = i.main;
-      } else if (!i.isFn && isCaps && isShift) {
-        i.down.TextContent = i.sup;
-      } else if (!i.isFn && !i.sup.innerHTML) {
-        i.down.TextContent = i.main;
-      }
-    });
-  } else {
-    arrButtons.forEach((u) => {
-      if (u.upper.TextContent && !u.isFn) {
-        u.upper.classList.remove('up-active');
-        u.down.classList.remove('up-inactive');
-        if (!isCaps) {
-          u.down.TextContent = u.sup;
-        } else if (isCaps) {
-          u.down.TextContent = u.main;
+        if (!i.isFn && isCaps && !isShift && !i.divUp.innerHTML) {
+          i.divMain.innerHTML = i.big;
+        } else if (!i.isFn && isCaps && isShift) {
+          i.divMain.innerHTML = i.small;
+        } else if (!i.isFn && !i.divUp.innerHTML) {
+          i.divMain.innerHTML = i.big;
         }
-      } else if (!u.isFn) {
-        if (isCaps) {
-          u.down.TextContent = u.main;
-        } else {
-          u.down.TextContent = u.sup;
+      });
+    } else {
+      arrButtons.forEach((u) => {
+        if (u.divUp.innerHTML && !u.isFn) {
+          u.divUp.classList.remove('up-active');
+          u.divMain.classList.remove('up-inactive');
+          if (!isCaps) {
+            u.divMain.innerHTML = u.small;
+          } else if (isCaps) {
+            u.divMain.innerHTML = u.big;
+          }
+        } else if (!u.isFn) {
+          if (isCaps) {
+            u.divMain.innerHTML = u.big;
+          } else {
+            u.divMain.innerHTML = u.small;
+          }
         }
-      }
-    });
-  }
+      });
+    }
+  };
 
-  function clearPress(x) {
-    if (!btnPress[x]) return;
-    if (!isCaps) { btnPress[x].div.classList.remove('pressed'); }
-
-    delete btnPress[x];
-  }
   const clearState = (ev) => {
+    const button = arrButtons.find((i) => i.code === ev.code);
     const { code } = ev.target.dataset;
     if (code === 'Shift') {
       isShift = false;
       upCase(false);
-      btnPress[code].div.classList.remove('pressed');
+      button[code].divBtn.classList.remove('pressed');
     }
     if (code === 'Control') { isCtrl = false; }
     if (code === 'Alt') {
       isAlt = false;
-      clearPress(code);
-      document.querySelector('.field-out').focus();
-      btnPress[code].div.removeEventListener('mouseleave', clearState);
+      if (button[code] && !isCaps) { button[code].divBtn.classList.remove('pressed'); }
+
+      outputArea.focus();
+      button[code].divBtn.removeEventListener('mouseleave', clearState);
     }
   };
 
   const outPrint = (btn, sml) => {
-    const out = document.querySelector('.field-out');
-    let pos = out.selectionStart;
-    const left = out.value.slice(0, pos);
-    const right = out.value.slice(pos);
+    // console.log(outputArea);
+    let pos = outputArea.selectionStart;
+    // console.log(pos);
+
+    const left = outputArea.value.slice(0, pos);
+    const right = outputArea.value.slice(pos);
 
     const navigationHandle = {
       Tab: () => {
-        out.value = `${left}\t${right}`;
+        outputArea.value = `${left}\t${right}`;
         pos += 1;
       },
       ArrowLeft: () => {
@@ -158,46 +141,48 @@ const renderKeyboard = (bool) => {
         pos += 1;
       },
       ArrowUp: () => {
-        const posLeft = out.value.slice(0, pos).match(/(\n).*$(?!\1)/g) || [
+        const posLeft = outputArea.value.slice(0, pos).match(/(\n).*$(?!\1)/g) || [
           [1],
         ];
         pos -= posLeft[0].length;
       },
       ArrowDown: () => {
-        const posLeft = out.value.slice(pos).match(/^.*(\n).*(?!\1)/) || [
+        const posLeft = outputArea.value.slice(pos).match(/^.*(\n).*(?!\1)/) || [
           [1],
         ];
         pos += posLeft[0].length;
       },
       Enter: () => {
-        out.value = `${left}\n${right}`;
+        outputArea.value = `${left}\n${right}`;
         pos += 1;
       },
       Delete: () => {
-        out.value = `${left}${right.slice(1)}`;
+        outputArea.value = `${left}${right.slice(1)}`;
       },
       Backspace: () => {
-        out.value = `${left.slice(0, -1)}${right}`;
+        outputArea.value = `${left.slice(0, -1)}${right}`;
         pos -= 1;
       },
       Space: () => {
-        out.value = `${left} ${right}`;
+        outputArea.value = `${left} ${right}`;
         pos += 1;
       },
     };
     if (navigationHandle[btn.code]) navigationHandle[btn.code]();
     else if (!btn.isFn) {
       pos += 1;
-      out.value = `${left}${sml || ''}${right}`;
+      outputArea.value = `${left}${sml || ''}${right}`;
     }
-    out.setSelectionRange(pos, pos);
+    outputArea.setSelectionRange(pos, pos);
   };
 
   const handleKeyboardEvent = (e) => {
     if (e.stopPropagation) e.stopPropagation();
     const btn = arrButtons.find((i) => i.code === e.code);
+    // console.log(arrButtons);
+
     if (!btn) return;
-    document.querySelector('.field-out').focus();
+    outputArea.focus();
 
     if (e.type === 'keydown' || e.type === 'mousedown') {
       if (!e.type === 'mousedown' && e.type === 'mouseup') { e.preventDefault(); }
@@ -206,20 +191,20 @@ const renderKeyboard = (bool) => {
       if ((e.code === 'Control' || e.code === 'Alt' || e.code === 'Caps') && (e.repeat)) { return; }
       if (e.code === 'Control') { isCtrl = true; }
       if (e.code === 'Alt') { isAlt = true; }
-      if (e.code === 'Control' && isAlt) {
-        // switchLan();
-      }
-      if (e.code === 'Alt' && isCtrl) {
-        //  switchLan();
-      }
-      btn.div.classList.add('pressed');
+      // if (e.code === 'Control' && isShift) {
+      //   switchLan();
+      // }
+      // if (e.code === 'Shift' && isCtrl) {
+      //   switchLan();
+      // }
+      btn.divBtn.classList.add('pressed');
       if (e.code === 'Caps' && !isCaps) {
         isCaps = true;
         upCase(true);
       } else if (e.code === 'Caps' && isCaps) {
         isCaps = false;
         upCase(false);
-        btn.div.classList.remove('pressed');
+        btn.divBtn.classList.remove('pressed');
       }
       if (!isCaps) {
         const symbolIsCaps = isShift ? btn.main : btn.sup;
@@ -233,29 +218,24 @@ const renderKeyboard = (bool) => {
           outPrint(btn, symbol);
         }
       }
-      btnPress[btn.e.code] = btn;
     } else if (e.type === 'keyup' || e.type === 'mouseup') {
-      clearPress(e.code);
-      btnPress[e.code].div.removeEventListener('mouseleave', clearState);
-
-      if (e.code === 'Shift') {
-        isShift = false;
-        upCase(false);
-      }
-      if (e.code === 'Control') { isCtrl = false; }
-      if (e.code === 'Alt') { isAlt = false; }
-
-      if (!e.code === 'Caps') btn.div.classList.remove('pressed');
+      if (btn[e.code] && !isCaps) { btn[e.code].divBtn.classList.remove('pressed'); }
     }
+
+    if (e.code === 'Shift') {
+      isShift = false;
+      upCase(false);
+    }
+    if (e.code === 'Control') { isCtrl = false; }
+    if (e.code === 'Alt') { isAlt = false; }
+
+    if (!e.code === 'Caps') btn.divBtn.classList.remove('pressed');
   };
 
   const handleMouseEvent = (e) => {
-    // console.log(e);
     e.stopPropagation();
     const targetBtn = e.target.closest('.input__key');
     if (!targetBtn) return;
-    // const { dataset: { code } } = targetBtn;
-    // console.log(targetBtn);
 
     targetBtn.addEventListener('mouseleave', clearState);
     handleKeyboardEvent({ code: e.code, type: e.type });
@@ -263,9 +243,9 @@ const renderKeyboard = (bool) => {
 
   document.addEventListener('keydown', handleKeyboardEvent);
   document.addEventListener('keyup', handleKeyboardEvent);
-  document.querySelector('.field-out').addEventListener('onmousedown', handleMouseEvent);
-  document.querySelector('.field-out').addEventListener('onmouseup', handleMouseEvent);
-  // document.querySelector('.field-out').onmousedown = handleMouseEvent;
-  // document.querySelector('.field-out').onmouseup = handleMouseEvent;
+  outputArea.addEventListener('onmousedown', handleMouseEvent);
+  outputArea.addEventListener('onmouseup', handleMouseEvent);
+  outputArea.onmousedown = handleMouseEvent;
+  outputArea.onmouseup = handleMouseEvent;
 };
 export default renderKeyboard;
