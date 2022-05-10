92754c49efd69e7a350174843d8e8bc2b194e41d
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-05-10 11:22:22 +0300

    fix: fix files

--- shelter/src/modules/addIdDataPets.js
+++
@@ -1,3 +0,0 @@
-import dataPets from "./db";
-const addIdDataPets = () => dataPets.forEach((pet, ind) => pet.id = ind)
-export default addIdDataPets
--- shelter/src/modules/burger.js
+++
@@ -1,7 +0,0 @@
-const menu = document.querySelector('.header__menu')
-
-const toggleMenu = () => {
-    menu.classList.toggle('burger-active')
-}
-
-burger.addEventListener("click", () => toggleMenu);
